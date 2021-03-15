5a1aa9cfdd49474c2939a0d6a15781464348d0af
alzbetasrutkova <61458211+alzbetasrutkova@users.noreply.github.com>
2021-03-15 17:40:09 +0100

    Add files via upload

--- Project3/sp_aprox_visual_studio_Alzbeta.py
+++ Project3/sp_aprox_visual_studio_Alzbeta.py
@@ -3,24 +3,38 @@ import sys
 import Bio
 from Bio import SeqIO
 
-def star_align(S, gap_cost,subst_mat):
+def star_align(seqs,subst_m, gap_cost):
+    subst_mat = read_subst_mtrx(subst_m)
+    S = dic_to_list(read_FASTA(seqs))
+    
     S1,S = find_center_seq(S, gap_cost, subst_mat)
-    #print(S1)
-    #print(S)
     #initialize M with the center seq:
+    #print("the center seq:")
+    #print(S1)
+    #print("the rest of the seqs:")
+    #print(S)
     M = split_arr(S1,1)
     for i in range(0,len(S)):
         n = len(S1)
-        m =  len(S[0])
+        m =  len(S[i])
         T = np.full([n+1,m+1], None)
         #unfortunately calculating the cost again:/
-        global_linear_cost(S1,S[0],gap_cost,subst_mat,T,n,m)
+        global_linear_cost(S1,S[i],gap_cost,subst_mat,T,n,m)
         #finding the optimal alignment between the center seq and another seq
-        A = IterBackTrack(S1,S[0],gap_cost,subst_mat,T)
+        A = IterBackTrack(S1,S[i],gap_cost,subst_mat,T)
+        #print("the current pairwise to be added:")
+        #print(A)
         #transposing the result, so that I work with columns rather than rows
         A = np.transpose(A)
-        M = extend(M,A)
+        M = extend(M,A)    
     return M
+    
+#helper function to translate a dictionary into a list of lists
+def dic_to_list(d):
+    l = []
+    for seq in d.values():
+        l.append(list(seq))
+    return l
 
 #helper function which splits an array arr to smaller arrays of a given size
 def split_arr(arr, size):
@@ -69,39 +83,28 @@ def extend(M,A):
         elif M[i][0] == '_' and A[j][0] != '_':
             M[i].append('_')
             i = i+1
-        #HERE IS THE PROBLEM
         elif M[i][0] != '_' and A[j][0] == '_':
-            c = np.full([len(M[i])], '_', dtype=object)
-            ch = A[j][1]
-            #c = np.append(c,ch)
-            print(c)
-            M.insert(i,c.tolist())
-            '''
-            gap_column = np.full([len(M[i])], '_')
-            print(gap_column)
-            gap_column = np.append(gap_column,A[j][1])
-            #gap_column.append(A[j][1])
-            print(gap_column)
-            M.insert(i,gap_column.tolist())
-            '''
+            new_column = np.full([len(M[i])], '_', dtype=object)
+            new_column = np.append(new_column,A[j][1])
+            M.insert(i,new_column.tolist())
             j = j+1
+            #because I actually shifted the order while inserting
+            i = i+1
         elif M[i][0] != '_' and A[j][0] != '_':
             M[i].append(A[j][1])
             i = i+1
             j = j+1
     #take care of the case, when you run out of one of the alignments -> add gaps
-    #HERE WILL PROBABLY BE THE SAME PROBLEM
     if i<len(M):
         while i<len(M):
             M[i].append('_')
             i = i+1
     if j<len(A):
         while j<len(A):
-            gap_column = np.full([len(M[0])], '_')
-            np.append(gap_column, A[j])
-            M.append(gap_column.tolist())
-            j = j+1
-            
+            new_column = np.full([len(M[0])-1], '_', dtype=object)
+            new_column = np.append(new_column, A[j][1])
+            M.append(new_column.tolist())
+            j = j+1            
     return M
 
 
@@ -164,10 +167,26 @@ def read_subst_mtrx(filename):
     return subst_mat 
 
 ### main ###
+seqs = sys.argv[1]
+subst_m = sys.argv[2]
+gap_cost = int(sys.argv[3])
+
+alignment = star_align(seqs,subst_m,5)
+alignment = np.transpose(alignment)
+
+for i in range(0,3):
+    print(''.join(alignment[i]))
+
+
+'''
 
 S = [['G','T','T','C','C','G','A','A','A','G','G','C','T','A','G','C','G','C','T','A','G','G','C','G','C','C'], 
     ['A','T','G','G','A','T','T','T','A','T','C','T','G','C','T','C','T','T','C','G'],
     ['T','G','C','A','T','G','C','T','G','A','A','A','C','T','T','C','T','C','A','A','C','C','A']]
+
+S = [['A','A','T','C','C','C','C'],
+['G','A','T','C','C','C','C'],
+['A','T']]
 
 
 subst_mat = np.array([
@@ -180,14 +199,11 @@ subst_mat = np.array([
 alignment = star_align(S,5,subst_mat)
 alignment = np.transpose(alignment)
 
-M_str = []
 for i in range(0,3):
     print(''.join(alignment[i]))
+'''
 
-print(M_str)
-
-
-#What is left here: debug the extend() method
+#What is left here: 
 #                   test if it works
 #                   make it runable from command line 
 
