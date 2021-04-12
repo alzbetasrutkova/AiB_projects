22e83ee03193032b0344639768c52a436a816b19
alzbetasrutkova <61458211+alzbetasrutkova@users.noreply.github.com>
2021-04-12 17:23:07 +0200

    Update rfdist_alzbeta.py
    attempt at step 4

--- Project4/rfdist_alzbeta.py
+++ Project4/rfdist_alzbeta.py
@@ -1,11 +1,29 @@
 from ete3 import Tree
+import numpy as np
 import sys
 
-def get_node_names(tree):
+def get_node_names(t):
     node_names = list()
-    for node in tree:  
+    for node in t:  
         node_names.append(node.name)
     return node_names
+
+def dfs_numbering(t):
+    depth_first_names = []
+    for node in t.traverse("preorder"):
+        if node.name != "":
+            depth_first_names.append(node.name)
+    return(depth_first_names)
+
+def get_df_intervals(t, df_num):
+    inters = list()
+    for node in t.traverse("preorder"):
+        if node.name == "":
+            min = 0, max = 0, size = 0
+            for n in node.traverse():
+                if 
+            
+    return(inters)
 
 
 ### main ###
@@ -18,7 +36,23 @@ tree2 = Tree(filename2)
 nodes_tree1 = get_node_names(tree1)
 root = nodes_tree1[0]
 
+###Root the trees
 #print(tree1)
 tree1.set_outgroup(root)
 #print(tree1)
 tree1.set_outgroup(root)
+
+###Get the DFS numbering
+df_tree1 = dfs_numbering(tree1)
+#print(df_tree1)
+df_tree2 = dfs_numbering(tree2)
+#print(df_tree2)
+
+###Get the DF-intervals
+
+
+
+
+
+
+
