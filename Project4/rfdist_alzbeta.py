3c83ead8a6697e21c620a6d71ee43e19450563ca
alzbetasrutkova <61458211+alzbetasrutkova@users.noreply.github.com>
2021-04-12 12:16:53 +0200

    Update rfdist_alzbeta.py
    get node names

--- Project4/rfdist_alzbeta.py
+++ Project4/rfdist_alzbeta.py
@@ -1,16 +1,24 @@
-from Bio import Phylo
+from ete3 import Tree
 import sys
+
+def get_node_names(tree):
+    node_names = list()
+    for node in tree:  
+        node_names.append(node.name)
+    return node_names
+
 
 ### main ###
 filename1 = sys.argv[1]
 filename2 = sys.argv[2]
 
-tree1 = Phylo.read(filename1, "newick")
-tree2 = Phylo.read(filename2, "newick")
+tree1 = Tree(filename1)
+tree2 = Tree(filename2)
 
-Phylo.draw_ascii(tree1)
+nodes_tree1 = get_node_names(tree1)
+root = nodes_tree1[0]
 
-tree1.root_with_outgroup({"name": "seq5"})
-Phylo.draw_ascii(tree1)
-
-
+#print(tree1)
+tree1.set_outgroup(root)
+#print(tree1)
+tree1.set_outgroup(root)
