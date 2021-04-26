c01a9074c69456b47872e03993e778978925fee7
alzbetasrutkova <61458211+alzbetasrutkova@users.noreply.github.com>
2021-04-26 10:01:16 +0200

    Add files via upload

---
+++ Project5/nj_alzbeta.py
@@ -0,0 +1,38 @@
+import sys
+import numpy as np
+from ete3 import Tree
+
+
+#an improved version of the matrix reader for project_3
+def read_subst_mtrx(filename):
+    f = open(filename,'r')
+    num_species = int(f.readline())
+
+    dict_mat = {}
+    mat = np.zeros((num_species,num_species))
+    
+    for i in range(0,num_species):
+        line = f.readline()
+        nums_in_line = line.split()
+        dict_mat[nums_in_line[0]] = i
+        for j in range(1,num_species):
+            mat[i,j-1] = nums_in_line[j]
+    f.close()
+    return dict_mat, mat 
+
+
+def tree_contruct_nj(dict_subst, mat ):
+    start = True
+
+    while(dict_mat.size > 3):
+        if start:
+            mat = 
+            start = False
+            pass
+        else:
+
+
+### main ###
+matr_filename = "C:\\Users\\alzbe\\Documents\\AU_Bioinfo_Masters\\Spring_2021\\AiB\\Projects\\Project_05\\example_slide4.phy"
+
+print(read_subst_mtrx(matr_filename))
