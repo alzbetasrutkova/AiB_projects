1a0a8702e02f3ce4c24aac6a603aa8e4a550e712
au679516 <lenab@DESKTOP-VM0FIGR.localdomain>
2021-04-30 14:29:40 +0200

    NJ timesreversed list

--- Project5/NJ_times.py
+++ Project5/NJ_times.py
@@ -3,22 +3,22 @@
 import os
 import time
 
-dist_matrix = ['1347_FAINT.phy',
- '1493_Fe-ADH.phy',
- '1560_Ferritin.phy',
- '1689_FGGY_N.phy',
- '1756_FAD_binding_3.phy',
- '1849_FG-GAP.phy',
- '214_Arena_glycoprot.phy',
- '304_A1_Propeptide.phy',
- '401_DDE.phy',
- '494_Astro_capsid.phy',
- '608_Gemini_AL2.phy',
- '777_Gemini_V1.phy',
+['89_Adeno_E3_CR1.phy',
  '877_Glu_synthase.phy',
- '89_Adeno_E3_CR1.phy']
+ '777_Gemini_V1.phy',
+ '608_Gemini_AL2.phy',
+ '494_Astro_capsid.phy',
+ '401_DDE.phy',
+ '304_A1_Propeptide.phy',
+ '214_Arena_glycoprot.phy',
+ '1849_FG-GAP.phy',
+ '1756_FAD_binding_3.phy',
+ '1689_FGGY_N.phy',
+ '1560_Ferritin.phy',
+ '1493_Fe-ADH.phy',
+ '1347_FAINT.phy']
 
-times = []
+"""times = []
 for i in range(len(dist_matrix)):
     file = dist_matrix[i]
     order = "python3 NJ.py " + file
@@ -26,4 +26,4 @@ for i in range(len(dist_matrix)):
     os.system(order)
     times.append(time.time() - start_time)
     print(i,times[i])
-print (times)
+print (times)"""
