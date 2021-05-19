2154129d3cc69433feb2fbf1915a01da8d5a6803
alzbetasrutkova <61458211+alzbetasrutkova@users.noreply.github.com>
2021-05-19 09:42:52 +0200

    score function added

--- Project6/approx_fold.py
+++ Project6/approx_fold.py
@@ -52,167 +52,79 @@ def find_score_and_pairs(seq_odd_even, evens_left = True):
     return score, pairs
 
 
-##################Christian's code#######################
-class HPFold:
-
-    def __init__ (self, s):
-        legal = {'h':'h', 'p':'p', 'H':'h', 'P':'p'}
-        self.seq = []
-        i = 1
-        for c in s:
-            if c in legal.keys():
-                if legal[c] == 'h' and i % 2 == 0:
-                    self.seq.append('H')
-                else:
-                    self.seq.append(legal[c])
-                i = i + 1
-
-    def __len__ (self):
-        return len(self.seq)
-        
-    def SetRelFold (self, relfold):
-        """
-        Fold seq according to a description in relavtive format, i.e.
-        a sequence of {f,l,r}'s which describe each step as (f)orward,
-        (l)eft, or (r)ight.
-        """
-        turn = {'f':0, 'l':-1, 'r':1}
-        direction = {0:'e', 1:'s', 2:'w', 3:'n'}
-        absfold = []
-        curr = 0
-        for relstep in relfold:
-            absstep = (curr + turn[relstep]) % 4
-            absfold.append(direction[absstep])
-            curr = absstep
-        return self.SetAbsFold(absfold)
-
-    def SetAbsFold (self, absfold):
-        """
-        Fold seq according to a description in absolute format, i.e.
-        s sequence of {n,s,e,w}'s which describe each step as (n)orth,
-        (s)outh, (e)ast, or (w)est.
-        """
-        self.legal_fold = (True, 0)
-        self.grid = {}
-        self.grid[0,0] = [0]
-        i = j = self.min_i= self.max_i = self.min_j = self.max_j = 0
-        k = 1
-        for step in absfold:
-            if step == 'n':
-                i = i - 1
-            elif step == 's':
-                i = i + 1
-            elif step == 'e':
-                j = j + 1
-            elif step == 'w':
-                j = j - 1
-            if (i,j) in self.grid.keys():
-                self.legal_fold = (False, k)
-                self.grid[i,j].append(k)
-            else:
-                self.grid[i,j] = [k]
-            k = k + 1
-            self.min_i = min(i, self.min_i)
-            self.max_i = max(i, self.max_i)
-            self.min_j = min(j, self.min_j)
-            self.max_j = max(j, self.max_j)                        
-        return self.legal_fold[0]
-
-    def ContainNeighbors(self, l1, l2):
-        """
-        Returns true if there exists k1 in l1 and k2 in l2 such that
-        abs(k1-k2) is 1, i.e. if the indices in l1 and l2 contain a
-        pair of neighbors in seq.
-        """
-        res = False
-        for k1 in l1:
-            for k2 in l2:
-                if abs(k1-k2) == 1:
-                    res = True
-        return res
-
-    def ContainHHs(self, l1, l2):
-        """
-        Returns true if there exists k1 in l1 and k2 in l2 where there
-        is a 'h' at position k1 and k2 in seq, i.e. if the indices in
-        l1 and l2 contain a pair which can make a h-h bond.
-        """
-        res = False
-        for k1 in l1:
-            for k2 in l2:
-                if (self.seq[k1] == "h" or self.seq[k1] == "H") and (self.seq[k2] == "h" or self.seq[k2] == "H"):
-                    res = True
-        return res
-
-    def PrintFold (self):
-        """
-        Print fold and output its score
-        """
-        score = 0
-        #print()
-        for i in range(self.min_i, self.max_i+1):
-            for j in range(self.min_j, self.max_j+1):
-                if (i,j) in self.grid.keys():
-                    l1 = self.grid[i,j]
-                    if len(l1) == 1:
-                        pass
-                        #print(self.seq[l1[0]], end="")
-                    else:
-                        pass
-                        #print("X", end="")
-                    if (i,j+1) in self.grid.keys():
-                        l2 = self.grid[i,j+1]
-                        if self.ContainNeighbors(l1,l2):
-                            pass
-                            #print("-", end="")
-                        elif self.ContainHHs(l1, l2):
-                            #print("*", end="")
-                            score = score + 1
-                        else:
-                            pass
-                            #print(" ", end="")
-                    else:
-                        pass
-                        #print(" ", end="")
-                else:
-                    pass
-                    #print(".", end="")
-                    #print(" ", end="")
+##################Lena's code#######################
+def score(seq, fold):
+    if seq[0]=="h" or seq[0]=="H":
+        grid={(0,0):0}
+    else : 
+        grid = {}
+    i=0
+    j=0
+    flag = "right"
+    for k in range(len(fold)):
+        if fold[k]=="f" : 
+            if flag == "right":
+                i+=1
+            elif flag=="left":
+                i-=1
+            elif flag == "up":
+                j+=1
+            else : 
+                j-=1
             
+        elif fold[k]=="l":
+            if flag == "right":
+                j+=1
+                flag = "up"
+            elif flag=="left":
+                j-=1
+                flag = "down"
+            elif flag == "up":
+                i-=1
+                flag="left"
+            else : 
+                i+=1
+                flag = "right"
+            
+        else : #fold[k]=="r"
+            if flag == "right":
+                j-=1
+                flag = "down"
+            elif flag=="left":
+                j+=1
+                flag = "up"
+            elif flag == "up":
+                i+=1
+                flag = "right"
+            else : 
+                i-=1
+                flag = "left"
+            
+        # If it is an "h", we add it to the dictionnary because it can be used for the score
+        if seq[k+1]=="h" or seq[k+1]=="H":
+            if seq[k]=='h'or seq[k]=='H':
+                grid[(i,j)]= k+1
+            else : 
+                grid[(i,j)]= k+1
+        
+     # Calculate the score : 
+    keys = list(grid.keys())
+    score = 0
+    for k in range(len(keys)):
+        i,j = keys[k]
+        if (i-1,j) in grid and grid[i-1,j]%2!=grid[i,j]%2 and grid[i-1,j]!=grid[i,j]-1 and grid[i-1,j]!=grid[i,j]+1: 
+            # the 2 last conditions are to avoid scoring H-h that are linked
+            score+=1
+        if (i,j+1) in grid and grid[i,j]%2 != grid[i,j+1]%2 and grid[i,j]!=grid[i,j+1]-1 and grid[i,j]!=grid[i,j+1]+1: 
+            # the 2 last conditions are to avoid scoring H-h that are linked
+            score +=1
+    return(score)
 
-            for j in range(self.min_j, self.max_j+1):
-                if (i,j) in self.grid.keys() and (i+1,j) in self.grid.keys():
-                    l1 = self.grid[i,j]
-                    l2 = self.grid[i+1,j]
-                    if self.ContainNeighbors(l1,l2):
-                        pass
-                        #print("|", end="")
-                    elif self.ContainHHs(l1,l2):
-                        #print("*", end="")
-                        score = score + 1
-                    else:
-                        pass
-                        #print(" ", end="")
-                else:
-                    pass
-                    #print(" ", end="")
-                #print(" ", end="")
-            #print()
-
-        if self.legal_fold[0]:
-            print("Score: %d" % (score))
-        else:
-            print("Illegal fold after %d steps" % (self.legal_fold[1]))
-       
 
 #####################################################################
 # Main
 #####################################################################
 seq_in = sys.argv[1]
-
-#seq_in = "hphhhhhh"
-#seq_in = "hhppppphhppphppphp"
-#seq_in = "hphphhhppphhhhpphh"
 
 #assign the odd/even labels
 seq_odd_even = odds_evens(seq_in)
@@ -305,11 +217,8 @@ if len(directions) == len(seq_in):
 #print(seq_in)
 print("Fold:", directions)
 
-seq = HPFold(seq_in)
-
-seq.SetRelFold(directions)
-
-seq.PrintFold()
+score = score(seq_in, directions)
+print("Score: ", score)
 
              
 
